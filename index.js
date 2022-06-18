74fec0ab88d671c190b6cd5df0c954c185e23d5c
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-06-18 13:32:18 +0300

    commit tree 1bb553d3f03ccceba20cdbf0ab284826d1322d58

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-function isPalindrome(word) {
+function isPalindrome(string) {
   // Write your algorithm here
+  return string.split('').reverse('').join('') === string ? true : false;
 }
 
+  
 /* 
   Add your pseudocode here
+
+  First split the string with .split(), then reverse it and 
+  finally join it back together.
+
 */
 
 /*
   Add written explanation of your solution here
+
+  After joining, is it equal to the string? If yes return true, 
+  else false.
+
 */
 
 // You can run `node index.js` to view these console logs
